5bea89bdd1c9e1838942328c47a991a2322bdcd7
putkarsh81 <putkarsh624@gmail.com>
2026-06-14 06:11:09 +0530

    Implement user login with JWT authentication

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ pool.connect()
 
 
 app.post('/register', require('./src/controller/authcontroller/register').registerUser);
+app.post('/login', require('./src/controller/authcontroller/login').Loginuser);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port ' + (process.env.PORT || 3000));
---
+++ backend/src/controller/authcontroller/login.js
@@ -0,0 +1,60 @@
+const pool = require('../../db/dbconnection');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const Loginuser = async (req,res) => {
+    let {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            msg: "All fields are required"
+        });
+    }
+
+    try{
+        const user = await pool.query(
+        "SELECT * FROM user_details WHERE email=$1", [email]
+    );
+
+    if(user.rows.length === 0){
+        return res.status(401).json({
+            "msg":"Invalid email or password"
+        });
+    }
+    const existingUser = user.rows[0];
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if(!isMatch){
+        return res.status(401).json({
+            "msg":"Invalid email or password"
+        });
+    }
+
+    const token = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    },
+        process.env.JWT_SECRET, {expiresIn: '1h'});
+
+    return res.status(200).json({
+        "msg":"Login successful",
+        "token": token,
+        user: {
+        id: existingUser.id,
+        username: existingUser.username,
+        email: existingUser.email
+    }
+    });
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({
+            "msg":"Internal server error"
+        });
+    }
+
+}
+
+module.exports = {
+    Loginuser
+}
